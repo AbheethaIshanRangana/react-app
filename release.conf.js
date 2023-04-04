7c0fddf2bdc8770f0b4452787952f310fe86c911
Abheetha Ishan Rangana <d.m.abheethaishan@gmail.com>
2023-04-04 10:07:00 +0530

    fix: Fix error in release.conf.js file

--- release.conf.js
+++ release.conf.js
@@ -1,9 +1,16 @@
-modeule.exports = {
+module.exports = {
     branches: "master",
     repositoryUrl: "https://github.com/AbheethaIshanRangana/react-app",
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
-        '@semantic-release/github'
+        ['@semantic-release/github',
+        {
+            assests: [
+                { path: "build.zip", label: "Build" },
+                { path: "coverage.zip", label: "Coverage" }
+            ]    
+        }
+        ]
     ]
-}
+};
